4111d2763ba63c7b81bf4edeab2e71c9939100be
mikealfare <alfare@gmail.com>
2021-12-27 23:07:34 -0500

    day 2 complete

--- advent_of_code/day_02.py
+++ advent_of_code/day_02.py
@@ -3,25 +3,50 @@ from pathlib import Path
 from math import prod
 
 
-Position = Tuple[int, int]
+Position = int
+Depth = int
+Aim = int
 Command = Tuple[str, int]
 
 
-def moves(position: Position, commands: Tuple[Command, ...]) -> Position:
+def moves(position: Position, depth: Depth, commands: Tuple[Command, ...]) -> Tuple[Position, Depth]:
     for command in commands:
-        position = move(position, command)
-    return position
+        position, depth = move(position, depth, command)
+    return position, depth
 
 
-def move(position: Position, command: Command) -> Position:
-    horizontal, depth = position
+def moves_with_aim(
+        position: Position,
+        depth: Depth,
+        aim: Aim,
+        commands: Tuple[Command, ...]
+) -> Tuple[Position, Depth, Aim]:
+    for command in commands:
+        position, depth, aim = move_with_aim(position, depth, aim, command)
+    return position, depth, aim
+
+
+def move(position: Position, depth: Depth, command: Command) -> Tuple[Position, Depth]:
     direction, units = command
     if direction == "forward":
-        return horizontal + units, depth
+        return position + units, depth
     elif direction == "up":
-        return horizontal, depth - units
+        return position, depth - units
     elif direction == "down":
-        return horizontal, depth + units
+        return position, depth + units
+    else:
+        raise ValueError(f"Invalid command, received: {direction}")
+
+
+
+def move_with_aim(position: Position, depth: Depth, aim: Aim, command: Command) -> Tuple[Position, Depth, Aim]:
+    direction, units = command
+    if direction == "forward":
+        return position + units, depth + aim * units, aim
+    elif direction == "up":
+        return position, depth, aim - units
+    elif direction == "down":
+        return position, depth, aim + units
     else:
         raise ValueError(f"Invalid command, received: {direction}")
 
@@ -37,4 +62,6 @@ def read_commands(file_name: str) -> Tuple[Command, ...]:
 
 
 if __name__ == "__main__":
-    print(prod(moves((0, 0), read_commands("day_02.txt"))))
+    print(prod(moves(0, 0, read_commands("day_02.txt"))))
+    position, depth, _ = moves_with_aim(0, 0, 0, read_commands("day_02.txt"))
+    print(position*depth)
--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -3,18 +3,18 @@ import pytest
 from advent_of_code import day_02
 
 
-@pytest.mark.parametrize("position,command,expected_position", [
-    ((0, 0), ("forward", 5), (5, 0)),
-    ((0, 0), ("down", 5), (0, 5)),
-    ((0, 0), ("up", 3), (0, -3)),
+@pytest.mark.parametrize("position,depth,command,expected", [
+    (0, 0, ("forward", 5), (5, 0)),
+    (0, 0, ("down", 5), (0, 5)),
+    (0, 0, ("up", 3), (0, -3)),
 ])
-def test_move(position, command, expected_position):
-    assert day_02.move(position, command) == expected_position
+def test_move(position, depth, command, expected):
+    assert day_02.move(position, depth, command) == expected
 
 
-@pytest.mark.parametrize("position,commands,expected_position", [
+@pytest.mark.parametrize("position,depth,commands,expected", [
     (
-        (0, 0),
+        0, 0,
         (
             ("forward", 5),
             ("down", 5),
@@ -26,5 +26,35 @@ def test_move(position, command, expected_position):
         (15, 10)
     )
 ])
-def test_moves(position, commands, expected_position):
-    assert day_02.moves(position, commands) == expected_position
+def test_moves(position, depth, commands, expected):
+    assert day_02.moves(position, depth, commands) == expected
+
+
+@pytest.mark.parametrize("position,depth,aim,command,expected", [
+    (0, 0, 0, ("forward", 5), (5, 0, 0)),
+    (5, 0, 0, ("down", 5), (5, 0, 5)),
+    (5, 0, 5, ("forward", 8), (13, 40, 5)),
+    (13, 40, 5, ("up", 3), (13, 40, 2)),
+    (13, 40, 2, ("down", 8), (13, 40, 10)),
+    (13, 40, 10, ("forward", 2), (15, 60, 10)),
+])
+def test_move_with_aim(position, depth, aim, command, expected):
+    assert day_02.move_with_aim(position, depth, aim, command) == expected
+
+
+@pytest.mark.parametrize("position,depth,aim,commands,expected", [
+    (
+        0, 0, 0,
+        (
+            ("forward", 5),
+            ("down", 5),
+            ("forward", 8),
+            ("up", 3),
+            ("down", 8),
+            ("forward", 2)
+        ),
+        (15, 60, 10)
+    )
+])
+def test_moves_with_aim(position, depth, aim, commands, expected):
+    assert day_02.moves_with_aim(position, depth, aim, commands) == expected
